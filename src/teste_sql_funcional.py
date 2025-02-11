e5305a9bff5aeca52fa360b90591ef86f0f84d6c
Th1agoFarias <th.silva.farias@gmail.com>
2025-02-10 22:11:55 -0300

    add sql url

--- src/teste_sql_funcional.py
+++ src/teste_sql_funcional.py
@@ -11,8 +11,12 @@ load_dotenv()
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT","5432")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+# Verifique se as variáveis foram carregadas corretamente
+if not all([POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DB]):
+    raise ValueError("Uma ou mais variáveis de ambiente não foram configuradas corretamente.")
 
 DATABASE_URL = (
     f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
